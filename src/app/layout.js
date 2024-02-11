2c539c5c1a9c78732f1b144ae2046a65cd7dd7fa
rezarinaldi <rez4rinaldi@gmail.com>
2024-02-11 21:34:21 +0700

    feat(dashboard): adding header, hero, and footer ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
